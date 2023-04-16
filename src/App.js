a023520a9e1b42b49d20b9a53559e11316b4bcf5
K-Maksimovich <carpovmax04@gmail.com>
2023-04-16 19:40:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
 
         <Routes>
                       {/* pages sites */}
-          <Route path="/deputies-of-nazarbayev/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/info" element={<Information />} />
           <Route path="/games" element={<Game />} />
 
