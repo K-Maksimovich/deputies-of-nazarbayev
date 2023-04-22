526ff7c18ca36a29b7d371b056daeb2bbc8d9925
K-Maksimovich <carpovmax04@gmail.com>
2023-04-22 21:30:06 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 import './reset.css'
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     
   </React.StrictMode>
 );
